9c6e462c719539f553b4c50fa24b1061201e0e0f
Vivek1034 <163897467+Vivek1034@users.noreply.github.com>
2025-09-12 21:38:08 +0530

    commit

--- Agri/login.js
+++ Agri/login.js
@@ -1,43 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+function handleRegister(event) {
+	event.preventDefault();
+	alert("Registration Successful! Redirecting to Login...");
+	window.location.href = "login.html";
+}
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBzgNpV6aFyjtDX6Ig9rgP8x2w647jWN5M",
-    authDomain: "smart-crop-advisory-f3913.firebaseapp.com",
-    projectId: "smart-crop-advisory-f3913",
-    storageBucket: "smart-crop-advisory-f3913.firebasestorage.app",
-    messagingSenderId: "231721730375",
-    appId: "1:231721730375:web:a07c0b372e8b99ed27cc62"
-};
+function handleLogin(event) {
+	event.preventDefault();
+	alert("Login Successful! Redirecting to Dashboard...");
+	window.location.href = "main.html";
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-// inputs
+function togglePassword() {
+	const passwordInput = document.getElementById("password");
+	const eyeIcon = document.getElementById("password-eye");
 
-
-// button
-const login = document.getElementById('loginButton');
-login.addEventListener("click", function (event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            alert("Logging In...")
-            window.location.href = "dashboard.html";
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage)
-            // ..
-        });
-})
+	if (passwordInput.type === "password") {
+		passwordInput.type = "text";
+		eyeIcon.className = "fas fa-eye-slash";
+	} else {
+		passwordInput.type = "password";
+		eyeIcon.className = "fas fa-eye";
+	}
+}
